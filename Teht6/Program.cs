4faef0babab39914184f3d73a9c73eb9d366854a
K2267 <K2267@student.jamk.fi>
2017-01-19 10:17:46 +0200

    teht 6 done

---
+++ Teht6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance;
+            Console.Write("How many km's you have drived? >> ");
+            distance = double.Parse(Console.ReadLine());
+
+            double consumption = distance / 100 * 7.02;
+            double cost = consumption * 1.595;
+            Console.WriteLine("Fuel consumption " + consumption + " liters, " + " costs " + cost + " euros");
+        }
+    }
+}
