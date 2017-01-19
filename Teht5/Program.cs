15e4f76cc8915b0ee9726cf6102727f62b38e880
K2267 <K2267@student.jamk.fi>
2017-01-19 10:03:34 +0200

    täsäs

---
+++ Teht5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int seconds;
+            Console.Write("Seconds? ");
+            seconds = int.Parse(Console.ReadLine());
+
+            int hours = seconds / 3600;
+            int min = seconds / 60 - hours * 60;
+            int sec = seconds % 60;
+            Console.WriteLine("Tunnit " + hours + " minuutit " + min + " sekuntit " + sec);
+        }
+    }
+}
