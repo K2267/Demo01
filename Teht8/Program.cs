f566c206e886e6699e77b777488b9af054543f07
K2267 <K2267@student.jamk.fi>
2017-01-24 11:35:14 +0200

    Teht8 done

---
+++ Teht8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Give 3 numbers
+            Console.WriteLine("Give me three numbers >> ");
+            int num1;
+            Console.Write("Give number1 > ");
+            num1 = int.Parse(Console.ReadLine());
+
+            int num2;
+            Console.Write("Give number2 > ");
+            num2 = int.Parse(Console.ReadLine());
+
+            int num3;
+            Console.Write("Give number3 > ");
+            num3 = int.Parse(Console.ReadLine());
+
+            //Solving biggest number
+            if (num1 > num2 && num2 > num3)
+            {
+                Console.WriteLine("Biggest number is: {0}", num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("Biggest number is: {0}", num2);
+            }
+            else
+            {
+                Console.WriteLine("Biggest number is: {0}", num3);
+            }
+            //Shorter way is to use array
+        }
+    }
+}
