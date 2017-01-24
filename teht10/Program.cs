bc009de294a7642525e8357df3b1be07fe421f51
K2267 <K2267@student.jamk.fi>
2017-01-24 15:29:29 +0200

    Teht10 muutoksia

--- teht10/Program.cs
+++ teht10/Program.cs
@@ -13,6 +13,9 @@ namespace teht10
             int[] numbers = { 1, 2, 33, 44, 55, 68, 77, 96, 100 }; //Taulukko
            
             for (int i = 0; i < numbers.Length; i++) Console.Write(numbers[i]); //Tulostaa taulukon
+                if (numbers[i] % 2 == 0)
+                    Console.Write("HEP");
+                else Console.Write(numbers[i]);
         }
     }
 }
