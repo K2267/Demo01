d91479e5ceb336f6716848227d85084d809bc6de
K2267 <K2267@student.jamk.fi>
2017-01-24 12:10:38 +0200

    tarkistus

---
+++ Teht9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int = number;
+            Console.Write("Give number > ");
+            number = int.Parse(Console.ReadLine());
+        }
+    }
+}
