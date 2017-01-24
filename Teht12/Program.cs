7f7ebf486ea78dfee2764062cc72df7fa2dfd024
K2267 <K2267@student.jamk.fi>
2017-01-24 21:40:00 +0200

    Teht12 done

---
+++ Teht12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5];
+            //Console.Write("Give a number > ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Give a number > ");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Reverse(nums); //Käännetään luvut toisinpäin taulukkoon
+            Console.Write(String.Join(", ", nums)); //Tulostetaan taulukko teksti muodossa
+
+            Console.ReadLine();
+        }
+    }
+}
