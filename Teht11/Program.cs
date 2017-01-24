3b90a1f898e66644ee1395054b85817a7cf56103
K2267 <K2267@student.jamk.fi>
2017-01-24 19:24:52 +0200

    Teht11 Done

---
+++ Teht11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using double for loop
+            int number;
+            Console.Write("How many rows * > ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
