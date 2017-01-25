ce1971657b949934d0b956b39cc6d7975cbbb85b
K2267 <K2267@student.jamk.fi>
2017-01-25 21:27:48 +0200

    Done

---
+++ Teht16/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random(); 
+
+            int number = random.Next(1, 100); //Valitaan rodomisti luku välillä 1-100
+            Console.WriteLine(number);
+
+            int Guess = 0;
+
+            Console.WriteLine("Guess number between 1-100. ");
+
+            for (int i = 1; i != 0; i++)
+            {
+                Guess = int.Parse(Console.ReadLine());
+
+                if (Guess < number)
+                {
+                    Console.WriteLine("No, the number I am thinking of is higher than " + Guess + ". Can you guess what it is?");
+                }
+                else if (Guess > number)
+                {
+                    Console.WriteLine("No, the number I am thinking of is lower than " + Guess + ". Can you guess what it is?");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! {0} is the correct number. You guessed number in {1} try! ", Guess, i);
+                
+                }
+
+            }   
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Teht17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int[] arr1 = { 5, 15, 25, 35, 45 };
+            int[] arrCombined = arr.Concat(arr1).ToArray(); //yhdistetään taulukot
+            Array.Sort(arrCombined); //Järjerstetään luvut
+            foreach (int value in arrCombined)
+            {
+                Console.Write(value);
+                // muuttuja on vuorossa oleva alkio kokoelmasta
+                Console.Write(","); // Erotetaan pilkulla
+            }
+        }
+    }
+}
